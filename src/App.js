d1cb913d84ca74cab968a61ca35eb717c69fe2b2
adnan <adnan.ros1@gmail.com>
2022-05-10 20:12:44 +0430

    init: Initialize with basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [persons, setPersons] = useState([
+    {
+      id: '4988988832',
+      name: 'Bill',
+      familyName: 'Gates',
+      birthDate: '1960/09/01'
+    },
+    {
+      id: '4988988833',
+      name: 'Jim',
+      familyName: 'Carry',
+      birthDate: '1965/09/01',
+    },
+    {
+      id: '4988988834',
+      name: 'John',
+      familyName: 'Stovart',
+      birthDate: '1970/09/01'
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className="main-box">
+          <div className='mb-3'>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+
+          {
+            persons.map((person, index) => (
+              <div key={index} className='d-flex' style={{ 'height': '150px'}}>
+                <div className='col-3' style={{ 'backgroundColor': 'red' }}>
+                  <img src={require('../src/images/bill-gates.jpg')} style={{ 'width': '100%', 'height': '100%' }} alt='...' />
+                </div>
+                <div className='col-9' style={{ 'backgroundColor': 'green' }}>
+                  {person.name}
+                </div>
+              </div>
+            ))
+          }
+
+
+
+        </div>
+
+      </div>
     </div>
   );
 }
