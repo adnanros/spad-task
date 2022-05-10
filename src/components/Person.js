78abd4805ad55335ea626b4ec32a8409446bca4c
adnan <adnan.ros1@gmail.com>
2022-05-10 20:27:52 +0430

    feat: add Person component

---
+++ src/components/Person.js
@@ -0,0 +1,14 @@
+const Person = ({person}) => {
+    return (
+        <div className='d-flex' style={{ 'height': '150px' }}>
+            <div className='col-3' style={{ 'backgroundColor': 'red' }}>
+                <img src={require('../images/bill-gates.jpg')} style={{ 'width': '100%', 'height': '100%' }} alt='...' />
+            </div>
+            <div className='col-9' style={{ 'backgroundColor': 'green' }}>
+                {person.name}
+            </div>
+        </div>
+    );
+}
+
+export default Person;
--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,16 +1,11 @@
+import Person from "./Person";
+
 const Persons = ({persons}) => {
     return (
         <div style={{'padding': '5px', 'backgroundColor': '#999'}}>
             {
                 persons.map((person, index) => (
-                    <div key={index} className='d-flex' style={{ 'height': '150px' }}>
-                        <div className='col-3' style={{ 'backgroundColor': 'red' }}>
-                            <img src={require('../images/bill-gates.jpg')} style={{ 'width': '100%', 'height': '100%' }} alt='...' />
-                        </div>
-                        <div className='col-9' style={{ 'backgroundColor': 'green' }}>
-                            {person.name}
-                        </div>
-                    </div>
+                    <Person key={index} person={person}/>
                 ))
             }
         </div>
