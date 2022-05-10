d743b35cf14f137125fcd18236bccda26464c521
adnan <adnan.ros1@gmail.com>
2022-05-11 02:19:23 +0430

    feat: add edit button and checkbox in position

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,23 +1,33 @@
 const Person = ({ person, onCheck }) => {
     return (
-        <div className='d-flex mb-3'
+        <div className='mb-3'
             style={{ 'height': '150px', 'padding': '3px', 'backgroundColor': '#666' }}>
-            <div className='col-3' style={{}}>
-                <img src={require('../images/bill-gates.jpg')} style={{ 'width': '100%', 'height': '100%' }} alt='...' />
-            </div>
-            <div className='col-9' style={{}}>
-                {person.name}
-                <div className='d-flex justify-content-between'>
-                    <div>
-                        <i class="fa fa-tag" aria-hidden="true"
-                            style={{ 'color': person.confirmed ? 'green' : 'red' }}></i>
+
+            <div className='d-flex'>
+                <div className='col-3' style={{}}>
+                    <img src={require('../images/bill-gates.jpg')} style={{ 'width': '100%', 'height': '100%' }} alt='...' />
+                </div>
+                <div className='col-9' style={{}}>
+                    <div className='d-flex justify-content-end'>
+                        <button className='btn btn-sm btn-ouline-secondary'>Edit</button>
+                        <div>
+                            <input type='checkbox' />
+                        </div>
                     </div>
-                    {!person.confirmed && 
-                    <div>
-                        <button className='btn btn-sm btn-primary' onClick={()=>{onCheck(person)}}>Check</button>
-                    </div>}
+                    {person.name}
+                    <div className='d-flex justify-content-between'>
+                        <div>
+                            <i class="fa fa-tag" aria-hidden="true"
+                                style={{ 'color': person.confirmed ? 'green' : 'red' }}></i>
+                        </div>
+                        {!person.confirmed &&
+                            <div>
+                                <button className='btn btn-sm btn-primary' onClick={() => { onCheck(person) }}>Check</button>
+                            </div>}
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 }
--- src/components/Persons.js
+++ src/components/Persons.js
@@ -2,7 +2,7 @@ import Person from "./Person";
 
 const Persons = ({persons, onCheck}) => {
     return (
-        <div style={{'padding': '5px', 'backgroundColor': '#999'}}>
+        <div style={{'padding': '5px', 'backgroundColor': '#999', 'marginBottom': '5px'}}>
             {
                 persons.map((person, index) => (
                 <Person key={index} person={person} onCheck={onCheck}/>
